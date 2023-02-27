c56c7dda1c3f5c8da716f9275c86602f43fe5204
Pratik Kumawat <kumawat10@gmail.com>
2023-02-27 07:23:22 +0000

    Component update

--- src/main/java/com/springboot/calculator/Calculator/CalculatorImpl.java
+++ src/main/java/com/springboot/calculator/Calculator/CalculatorImpl.java
@@ -1,10 +1,12 @@
 package com.springboot.calculator.Calculator;
 
+import org.springframework.boot.Component;
+
+@Component
 public class CalculatorImpl implements CalculatorInterface {
 
     @Override
     public int sum(int num1, int num2) {
-
         return num1 + num2;
     }
 }
